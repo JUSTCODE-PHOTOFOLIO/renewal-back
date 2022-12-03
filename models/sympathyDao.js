1fcfee0650193c9131cd4d1d88c90bfaf74e4863
c0dject <creseeds@gmail.com>
2022-12-04 07:34:15 +0900

    Refactor: 공감하기 service 처리 - 공감 여부에 따른 공감하기, 공감 수정하기에 대한 판단을 기존 Dao단에서 Service단으로 변경

--- models/sympathyDao.js
+++ models/sympathyDao.js
@@ -1,7 +1,7 @@
 const myDataSource = require('.');
 
-// 공감
-const createSympathy = async (posting_id, user_id, sympathy_id) => {
+// 피드에서 로그인유저의 공감여부 확인하기
+const findSympathyOfFeedByUser = async (posting_id, user_id) => {
   const checkSympathy = await myDataSource.query(
     `
       SELECT
@@ -14,69 +14,72 @@ const createSympathy = async (posting_id, user_id, sympathy_id) => {
     `,
     [posting_id, user_id]
   );
-  let checkValue = checkSympathy[0].check_cnt;
+  return checkSympathy[0].check_cnt;
+};
 
-  // TODO 서비스단으로 올리기
-  if (checkValue == 0) {
-    await myDataSource.query(
-      `
+// 공감하기
+const createSympathy = async (posting_id, user_id, sympathy_id) => {
+  await myDataSource.query(
+    `
         INSERT
-          INTO
-          Works_Sympathy_Count (user_id,
-          posting_id,
-          sympathy_id)
-        VALUES ( ?, ?, ?)
-      `,
-      [user_id, posting_id, sympathy_id]
-    );
-    const result = await myDataSource.query(
-      `
-        SELECT
-          *
+        INTO
+            Works_Sympathy_Count (user_id,
+                                  posting_id,
+                                  sympathy_id)
+        VALUES
+            (?, ?, ?)
+    `,
+    [user_id, posting_id, sympathy_id]
+  );
+  const result = await myDataSource.query(
+    `
+        SELECT *
         FROM
-          Works_Sympathy_Count wsc
+            Works_Sympathy_Count wsc
         WHERE
-          user_id = ?
+            user_id = ?
           AND posting_id = ?
-      `,
-      [user_id, posting_id, sympathy_id, user_id, posting_id]
-    );
+    `,
+    [user_id, posting_id, sympathy_id, user_id, posting_id]
+  );
 
-    const resultCount = await myDataSource.query(
-      `
-        WITH tables AS (
-          SELECT
-            wp.id AS id,
-            ws.sympathy_sort AS sympathy_sort,
-            IFNULL(COUNT(wsc.id), '0') AS sympathy_cnt
-          FROM
-            Works_Sympathy ws
-          LEFT JOIN Works_Sympathy_Count wsc ON
-            ws.id = wsc.sympathy_id
-          LEFT JOIN Users u ON
-            u.id = wsc.user_id
-          LEFT JOIN Works_Posting wp ON
-            wsc.posting_id = wp.id
-          WHERE
-            wp.id = ?
-          GROUP BY
-            ws.sympathy_sort
-          )
+  const resultCount = await myDataSource.query(
+    `
+        WITH
+            tables AS (SELECT
+                           wp.id                      AS id,
+                           ws.sympathy_sort           AS sympathy_sort,
+                           IFNULL(COUNT(wsc.id), '0') AS sympathy_cnt
+                       FROM
+                           Works_Sympathy ws
+                               LEFT JOIN Works_Sympathy_Count wsc ON
+                               ws.id = wsc.sympathy_id
+                               LEFT JOIN Users u ON
+                               u.id = wsc.user_id
+                               LEFT JOIN Works_Posting wp ON
+                               wsc.posting_id = wp.id
+                       WHERE
+                           wp.id = ?
+                       GROUP BY
+                           ws.sympathy_sort)
 
         SELECT
-          a.id,
-          ws.sympathy_sort,
-          IFNULL(a.sympathy_cnt, '0') AS sympathy_cnt
+            a.id,
+            ws.sympathy_sort,
+            IFNULL(a.sympathy_cnt, '0') AS sympathy_cnt
         FROM
-          Works_Sympathy ws
-        LEFT JOIN tables a ON
-          a.sympathy_sort = ws.sympathy_sort `,
-      [posting_id]
-    );
-    return { result, resultCount };
-  } else if (checkValue == 1) {
-    await myDataSource.query(
-      `
+            Works_Sympathy ws
+                LEFT JOIN tables a ON
+                a.sympathy_sort = ws.sympathy_sort `,
+    [posting_id]
+  );
+  return { result, resultCount };
+};
+
+// 공감 수정하기
+const updateSympathy = async (posting_id, user_id, sympathy_id) => {
+  await myDataSource.query(
+    `
       UPDATE
         Works_Sympathy_Count
       SET
@@ -85,10 +88,10 @@ const createSympathy = async (posting_id, user_id, sympathy_id) => {
         user_id = ?
         AND posting_id = ?
       `,
-      [sympathy_id, user_id, posting_id]
-    );
-    const result = await myDataSource.query(
-      `
+    [sympathy_id, user_id, posting_id]
+  );
+  const result = await myDataSource.query(
+    `
         SELECT
           *
         FROM
@@ -97,11 +100,11 @@ const createSympathy = async (posting_id, user_id, sympathy_id) => {
           user_id = ?
           AND posting_id = ?
       `,
-      [user_id, posting_id]
-    );
+    [user_id, posting_id]
+  );
 
-    const resultCount = await myDataSource.query(
-      `
+  const resultCount = await myDataSource.query(
+    `
       WITH
           tables AS (SELECT
              wp.id AS id,
@@ -129,10 +132,9 @@ const createSympathy = async (posting_id, user_id, sympathy_id) => {
               LEFT JOIN tables a ON
               a.sympathy_sort = ws.sympathy_sort
       `,
-      [posting_id]
-    );
-    return { result, resultCount };
-  }
+    [posting_id]
+  );
+  return { result, resultCount };
 };
 
 // 공감 취소
@@ -164,6 +166,8 @@ const deleteSympathy = async (posting_id, user_id) => {
 };
 
 module.exports = {
+  findSympathyOfFeedByUser,
   createSympathy,
+  updateSympathy,
   deleteSympathy,
 };
--- services/sympathyService.js
+++ services/sympathyService.js
@@ -2,7 +2,15 @@ const sympathyDao = require('../models/sympathyDao');
 
 // 공감
 const createSympathy = async (posting_id, user_id, sympathy_id) => {
-  return await sympathyDao.createSympathy(posting_id, user_id, sympathy_id);
+  const findSympathyOfFeedByUser = await sympathyDao.findSympathyOfFeedByUser(
+    posting_id,
+    user_id
+  );
+  if (findSympathyOfFeedByUser === '0') {
+    return await sympathyDao.createSympathy(posting_id, user_id, sympathy_id);
+  } else {
+    return await sympathyDao.updateSympathy(posting_id, user_id, sympathy_id);
+  }
 };
 
 // 공감취소
